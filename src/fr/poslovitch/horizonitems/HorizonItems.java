9833e0d6a7c575b63768f67cb881f6aec9558fe4
Poslovitch <poslovitch@gmail.com>
2017-10-28 13:29:18 +0200

    Files can now be loaded

--- src/fr/poslovitch/horizonitems/HorizonItems.java
+++ src/fr/poslovitch/horizonitems/HorizonItems.java
@@ -1,15 +1,19 @@
 package fr.poslovitch.horizonitems;
 
 import fr.poslovitch.horizonitems.items.ItemsManager;
+import fr.poslovitch.horizonitems.util.FileLister;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HorizonItems extends JavaPlugin {
 
-    public static HorizonItems instance;
+    private static HorizonItems instance;
+    private FileLister fileLister;
 
     @Override
     public void onEnable() {
         instance = this;
+        fileLister = new FileLister(this);
+
         getLogger().info("§eLoading Items & Blocks.");
         ItemsManager.loadItems();
         getLogger().info("§aLoaded a total of §b" + ItemsManager.all.size() + " §aitems and blocks.");
@@ -18,5 +22,13 @@ public class HorizonItems extends JavaPlugin {
     @Override
     public void onDisable() {
         instance = null;
+    }
+
+    public static HorizonItems getInstance() {
+        return instance;
+    }
+
+    public FileLister getFileLister() {
+        return fileLister;
     }
 }
--- src/fr/poslovitch/horizonitems/items/ItemsManager.java
+++ src/fr/poslovitch/horizonitems/items/ItemsManager.java
@@ -3,6 +3,7 @@ package fr.poslovitch.horizonitems.items;
 import fr.poslovitch.horizonitems.HorizonItems;
 import fr.poslovitch.horizonitems.util.FileLister;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +12,22 @@ public class ItemsManager {
     public static List<HorizonItem> all = new ArrayList<HorizonItem>();
 
     public static void loadItems() {
-        FileLister fl = new FileLister(HorizonItems.instance);
-        for(String f : fl.list("items", "hitem")) {
-            HorizonItems.instance.getLogger().info(f);
+        for(String item : HorizonItems.getInstance().getFileLister().list("items", "hitem")) {
+            File itemFile = new File(HorizonItems.getInstance().getDataFolder() + "/items", item + ".hitem");
+
+            // Firstly, check if this item comes from the JAR.
+            // Then we can copy it into the correct folder
+            if (!itemFile.exists()) {
+                if (HorizonItems.getInstance().getResource("items/" + item + ".hitem") != null) {
+                    HorizonItems.getInstance().saveResource("items/" + item + ".hitem", true);
+                    itemFile = new File(HorizonItems.getInstance().getDataFolder() + "/items", item + ".hitem");
+                }
+            }
+
+            // Now we can parse its data!
+            System.out.println(item);
+
+            
         }
     }
 }
